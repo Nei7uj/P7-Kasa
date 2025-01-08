de79a27065977344cac8a1617a735595d430294f
nei7uj <julien.dille@gmail.com>
2025-01-08 17:07:18 +0100

    page d'accueil

---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Navbar.css";
+
+function Navbar() {
+    return <nav className='navbar'>
+        <div className='navbar__logo'>
+            <img src='logo.png' alt ="logo"/>
+        </div>
+        <div>Accueil</div>
+        <div>A propos</div>
+    </nav>;
+}
+
+export default Navbar
