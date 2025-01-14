7cabe5e8828771681d44054a00dc47402141302b
nei7uj <julien.dille@gmail.com>
2025-01-14 14:38:25 +0100

    Router OK

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route, createBrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="flat" element={<h1>Nos appartements</h1>} />
+      <Route path="about" element={<h1>A propos</h1>} />
+      <Route path="error" element={<h1>404</h1>} />
+    </Routes>
+  </BrowserRouter>
+);
