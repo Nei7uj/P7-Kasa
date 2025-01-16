0e35e417d834592adca060e20e4dfb5739165d5e
nei7uj <julien.dille@gmail.com>
2025-01-16 12:50:56 +0100

    Rangement dossiers

---
+++ src/assets/pages/ApartmentPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ApartmentPage() {
+    return(
+        <div>
+        
+        </div>
+
+    ) 
+}
+
+export default ApartmentPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Navbar from './assets/components/Navbar'
-import Footer from './assets/components/Footer'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import HomePage from './assets/pages/HomePage';
+import Navbar from './assets/components/Navbar';
+import Footer from './assets/layout/Footer';
+import ApartmentPage from "./assets/pages/ApartmentPage";
 import { BrowserRouter, Routes, Route} from "react-router";
 
 const root = document.getElementById("root");
@@ -11,8 +11,8 @@ const root = document.getElementById("root");
 createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="flat" element={<><Navbar /><h1>Nos appartements</h1><Footer /></>} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="flat" element={<><Navbar /><ApartmentPage /><Footer /></>} />
       <Route path="about" element={<><Navbar /><h1>A propos</h1><Footer /></>} />
       <Route path="error" element={<h1>404</h1>} />
     </Routes>
