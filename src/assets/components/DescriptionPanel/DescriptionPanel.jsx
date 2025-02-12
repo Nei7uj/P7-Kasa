12722a1a4273fba29193c0255d9d313d2183a9c9
nei7uj <julien.dille@gmail.com>
2025-02-12 13:23:11 +0100

    Miss mobile version

--- src/assets/components/DescriptionPanel/DescriptionPanel.jsx
+++ src/assets/components/DescriptionPanel/DescriptionPanel.jsx
@@ -4,22 +4,27 @@ import "./DescriptionPanel.scss";
 function DescriptionPanel(props) {
     const [isContentVisible, setIsContentVisible] = useState(false);
 
-    const showContent = () => {
+    const toggleContent = () => {
         setIsContentVisible(!isContentVisible);
-    }
-    const contentClass = isContentVisible ? "visible" : "hidden";
+    };
+
     return (
         <div className="apartment__description">
-                    <p className="description__header">
-                        <span>{props.title}</span>
-                        <i class="fa-solid fa-chevron-down" onClick={showContent}></i>
-                    </p>
+            <p className="description__header">
+                <span>{props.title}</span>
+                <i 
+                    className={`fa-solid ${isContentVisible ? "fa-chevron-up" : "fa-chevron-down"}`} 
+                    onClick={toggleContent}
+                ></i>
+            </p>
 
-                    {isContentVisible &&<p className="description__content">
-                        {props.content}
-                        </p>}
-                </div>
-    )
+            {isContentVisible && (
+                <p className="description__content">
+                    {props.content}
+                </p>
+            )}
+        </div>
+    );
 }
 
-export default DescriptionPanel
+export default DescriptionPanel;
